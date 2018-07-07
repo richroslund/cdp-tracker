b6c13c6ab6b6f9bb81194bc4a9e0a091dc8a8fd1
Rich Roslund <rich.roslund@gmail.com>
2018-07-07 01:53:52 -0500

    hooking api up to kue

---
+++ tracker-service/src/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const axios = require('axios');
+const redis = require('redis');
+const app = express();
+
+var kue = require('kue')
+var queue = kue.createQueue({redis: process.env.REDIS_URL});
+// var db = redis.createClient(process.env.REDIS_URL);
+
+
+app.get('/', (req, res) => {
+  return res.json({ version: "0.0.1" });
+});
+app.get('/cdp', (req, res) => {
+  redis.createClient(process.env.REDIS_URL).hgetall("cdp", function (err, obj) {
+    console.dir(obj);
+    return obj;
+  });
+});
+
+
+app.post('/cdp/refresh', (req, res) => {
+  var job = queue.create('fetch-cdps').save( function(err){
+    if( err )
+      return res.json({error: err}, err);
+    return res.json({id: job.id}, job)
+  });
+});
+
+app.get('/cdp/:cdpid', (req, res) => {
+  console.dir(req.params.cdpid);
+  redis.createClient(process.env.REDIS_URL).hget("cdp", "req.params.cdpid", function (err, obj) {
+    return obj;
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
