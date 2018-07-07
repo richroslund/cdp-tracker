13df3b09b6fe620600ad1da54ee3b25792c19a6c
Rich Roslund <rich.roslund@gmail.com>
2018-07-06 23:40:04 -0500

    Adding basic styles

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Container  } from 'bloomer';
-import 'bulma/css/bulma.css';
+// import 'bulma/css/bulma.css';
 
 ReactDOM.render( <Container><App /></Container>, document.getElementById('root'));
 registerServiceWorker();
